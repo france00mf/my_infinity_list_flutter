d65159b8f24a9a9aa07845ecad31bc845a2a4f7a
France! <franciscodomingosjose06798@gmail.com>
2024-06-07 11:49:20 +0100

    finesh movie entite

--- lib/domain/entities/movies.dart
+++ lib/domain/entities/movies.dart
@@ -1,3 +1,30 @@
-class Media {
+import 'package:equatable/equatable.dart';
+
+class Media extends Equatable{
+  final int tmdbID;
+  final String title;
+  final String posterUrl;
+  final String backdropUrl;
+  final double voteAverage;
+  final String releaseDate;
+  final String overview;
+  final bool isMovie;
+
+  Media({required this.tmdbID, required this.title, required this.posterUrl, required this.backdropUrl, required this.voteAverage, required this.releaseDate, required this.overview, required this.isMovie});
   
+ 
+  
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+       tmdbID,
+        title,
+        posterUrl,
+        backdropUrl,
+        voteAverage,
+        releaseDate,
+        overview,
+        isMovie,
+  ];
+
 }
