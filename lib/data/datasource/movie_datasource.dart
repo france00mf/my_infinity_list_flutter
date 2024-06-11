560a3bc9c1a9a7ae439483b1a5d1ace4cf59707a
France! <franciscodomingosjose06798@gmail.com>
2024-06-11 09:20:54 +0100

    add methods for FromJson factory return on movie model

--- lib/data/datasource/movie_datasource.dart
+++ lib/data/datasource/movie_datasource.dart
@@ -1,18 +1,18 @@
 
 import 'package:dio/dio.dart';
-import 'package:my_infinity_list_flutter/domain/entities/movies.dart';
+import 'package:my_infinity_list_flutter/data/model/movie_model.dart';
 
 abstract class MoviesDatasource{
-  Future<List<Movie>> getAllPopularMovies(int page);
+  Future<List<MovieModel>> getAllPopularMovies(int page);
 }
 
 class MoviesRemoteDatasource extends MoviesDatasource{
   @override
-  Future<List<Movie>> getAllPopularMovies(int page) async{
+  Future<List<MovieModel>> getAllPopularMovies(int page) async{
     final response = await Dio().get("");
     
     if(response.statusCode==200){
-        return List<Movie>.from((response.data['results'] as List).map((e) => Movie.fromJson(e)));
+        return List<MovieModel>.from((response.data['results'] as List).map((e) => MovieModel.fromJson(e)));
     }else{
       throw "Erro do servidor";
     }
