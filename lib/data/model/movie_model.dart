ce0f6ff2332480d022b441e5361a555a89a09299
France! <franciscodomingosjose06798@gmail.com>
2024-06-12 06:56:06 +0100

    fix error on movie model

--- lib/data/model/movie_model.dart
+++ lib/data/model/movie_model.dart
@@ -16,14 +16,78 @@ class MovieModel extends Movie{
         );
 }
 
-getPosterUrl(json){
-  return json;
+var moviePlaceHolder;
+
+getPosterUrl(path){
+  var basePosterUrl;
+  if (path != null) {
+    return basePosterUrl + path;
+  } else {
+    return moviePlaceHolder;
+  }
 }
 
-getBackdropUrl(json){
-  return json;
+getBackdropUrl(path){
+  var baseBackdropUrl;
+    if (path != null) {
+    return baseBackdropUrl + path;
+  } else {
+    return moviePlaceHolder;
+  }
 }
 
-getDate(json){
-  return json;
+getDate(date){
+  if (date == null || date.isEmpty) {
+    return '';
+  }
+
+  final vals = date.split('-');
+  String year = vals[0];
+  int monthNb = int.parse(vals[1]);
+  String day = vals[2];
+
+  String month = '';
+
+  switch (monthNb) {
+    case 1:
+      month = 'Jan';
+      break;
+    case 2:
+      month = 'Feb';
+      break;
+    case 3:
+      month = 'Mar';
+      break;
+    case 4:
+      month = 'Apr';
+      break;
+    case 5:
+      month = 'May';
+      break;
+    case 6:
+      month = 'Jun';
+      break;
+    case 7:
+      month = 'Jul';
+      break;
+    case 8:
+      month = 'Aug';
+      break;
+    case 9:
+      month = 'Sep';
+      break;
+    case 10:
+      month = 'Oct';
+      break;
+    case 11:
+      month = 'Nov';
+      break;
+    case 12:
+      month = 'Dec';
+      break;
+    default:
+      break;
+  }
+
+  return '$month $day, $year';
 }
