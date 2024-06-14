b23ee598767f2e218106bec4815b1ad8b84fe90a
France! <franciscodomingosjose06798@gmail.com>
2024-06-14 14:50:12 +0100

    add init code for ui

---
+++ lib/presenter/ui/infinity_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InfinityView extends StatefulWidget {
+  const InfinityView({super.key});
+
+  @override
+  State<InfinityView> createState() => _InfinityViewState();
+}
+
+class _InfinityViewState extends State<InfinityView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
