05fa4667317b5cc381d22c6f4542a2a707bf3cd9
France! <franciscodomingosjose06798@gmail.com>
2024-06-16 08:49:15 +0100

    implement movie use case

--- lib/domain/use_case/movie_use_case.dart
+++ lib/domain/use_case/movie_use_case.dart
@@ -1,13 +1,16 @@
 
 import 'package:dartz/dartz.dart';
+import 'package:my_infinity_list_flutter/domain/entities/movies.dart';
+import 'package:my_infinity_list_flutter/domain/repository/movide_repository.dart';
 import 'package:my_infinity_list_flutter/errors/failure.dart';
 import 'package:my_infinity_list_flutter/usecase/base_use_case.dart';
 
-class MovieUseCase implements BaseUseCase{
+class MovieUseCase implements BaseUseCase<List<Movie>, int>{
+  final MovieRepository baseRepository;
+  MovieUseCase(this.baseRepository);
   @override
-  Future<Either<Failure, dynamic>> call(p) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<Failure, List<Movie>>> call(p) async{
+    return await baseRepository.getAllMovie(p);    
   }
 
 }
