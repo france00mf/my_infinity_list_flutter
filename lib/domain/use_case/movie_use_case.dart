1710fe8ebcb48068a4bfee8441924ecf7e00b5cc
France! <franciscodomingosjose06798@gmail.com>
2024-06-16 08:36:44 +0100

    update

--- lib/domain/use_case/movie_use_case.dart
+++ lib/domain/use_case/movie_use_case.dart
@@ -1,4 +1,13 @@
 
+import 'package:dartz/dartz.dart';
+import 'package:my_infinity_list_flutter/errors/failure.dart';
+import 'package:my_infinity_list_flutter/usecase/base_use_case.dart';
+
 class MovieUseCase implements BaseUseCase{
+  @override
+  Future<Either<Failure, dynamic>> call(p) {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
 
 }
