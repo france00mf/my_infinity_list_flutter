63a3fbe74d6566996ac4e315acb47489245858e3
France! <franciscodomingosjose06798@gmail.com>
2024-06-16 08:55:43 +0100

    add supers on infinity bloc controller

--- lib/presenter/controller/infinity_bloc.dart
+++ lib/presenter/controller/infinity_bloc.dart
@@ -1,10 +1,27 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_infinity_list_flutter/domain/use_case/movie_use_case.dart';
 import 'package:my_infinity_list_flutter/presenter/controller/infinity_event.dart';
 import 'package:my_infinity_list_flutter/presenter/controller/infinity_state.dart';
 
 class InfinityBloc extends Bloc<InfinityEvent,InfinityState>{
+ final MovieUseCase baseMovieUseCase;
  
- 
-  InfinityBloc(super.initialState);
+  InfinityBloc(this.baseMovieUseCase): super(InfinityState()){
+    on<GetInfinityEvent>(_getAllPopularMovies);
+    on<FetchMoreGetInfinityEvent>(_fetchMoreMovies);
+  }
 
+  Future<void> _getAllPopularMovies(
+    GetInfinityEvent event,
+    Emitter<InfinityState> emit
+  ){
+    return Future(() => null);
+  }
+
+  Future<void> _fetchMoreMovies(
+    FetchMoreGetInfinityEvent event,
+    Emitter<InfinityState> emit
+  ){
+    return Future(() => null);
+  }
 }
