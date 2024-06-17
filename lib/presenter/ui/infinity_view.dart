22e9947e307b41fac96df8b3946632be2ee9e749
France! <franciscodomingosjose06798@gmail.com>
2024-06-17 06:57:44 +0100

    minor changes on bloc

--- lib/presenter/ui/infinity_view.dart
+++ lib/presenter/ui/infinity_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_infinity_list_flutter/locator/locator.dart';
+import 'package:my_infinity_list_flutter/presenter/controller/infinity_bloc.dart';
+import 'package:my_infinity_list_flutter/presenter/controller/infinity_event.dart';
+import 'package:my_infinity_list_flutter/presenter/controller/infinity_state.dart';
 
 class InfinityView extends StatefulWidget {
   const InfinityView({super.key});
@@ -10,6 +15,15 @@ class InfinityView extends StatefulWidget {
 class _InfinityViewState extends State<InfinityView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create: (context) => locator<InfinityBloc>()..add(GetInfinityEvent()),
+      child: Scaffold(
+      body: BlocBuilder<InfinityBloc, InfinityState>(
+        
+        builder: (context,state){
+          // return 
+      }),
+      ),
+    );
   }
 }
