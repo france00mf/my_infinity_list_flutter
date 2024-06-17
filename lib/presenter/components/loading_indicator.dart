ef20940bc060ac8b5e1da3f264328a111427f8e6
France! <franciscodomingosjose06798@gmail.com>
2024-06-17 14:42:51 +0100

    add loading indicator

---
+++ lib/presenter/components/loading_indicator.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndicator extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const CircularProgressIndicator(
+      color: Color(0xffef233c),
+    );
+  }
+
+}
