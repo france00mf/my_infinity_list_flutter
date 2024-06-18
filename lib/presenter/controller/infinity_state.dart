3f5adf118ce5dd1e825669cef3dbb2b42deca8f1
France! <franciscodomingosjose06798@gmail.com>
2024-06-18 09:35:48 +0100

    finesh InfinityState bloc imp

--- lib/presenter/controller/infinity_state.dart
+++ lib/presenter/controller/infinity_state.dart
@@ -2,11 +2,37 @@
 
 
 import 'package:equatable/equatable.dart';
+import 'package:my_infinity_list_flutter/domain/entities/movies.dart';
+import 'package:my_infinity_list_flutter/utils/enums.dart';
 
 class InfinityState extends Equatable{
+  final List<Movie> movies;
+  final GetAllRequestStatus status;
+  final String message;
+
+  const InfinityState(
+    {
+       this.movies = const [],
+    this.status = GetAllRequestStatus.loading,
+    this.message = '',
+    }
+  );
+
+  InfinityState copyWith({
+    List<Movie>? movies,
+    GetAllRequestStatus? status,
+    String? message,
+  }) => InfinityState(message: message ?? this.message, status: status ??this.status, movies: movies?? this.movies);
+
+
+
   @override
   // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [
+        movies,
+        status,
+        message,
+  ];
 
 }
 
